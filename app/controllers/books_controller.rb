16aaf4bc7535de8273e5f4ec15276f335d1d0954
Ryo Utsunomiya <millionsnowliving@gmail.com>
2016-06-26 13:41:28 +0900

    Add basic CRUD functions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,48 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  # POST /books
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book, notice: '本を登録しました。'
+    else
+      render :new
+    end
+  end
+
+  # GET/books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/:id/edit
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  # GET /books/:id
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  # PATCH /books/:id
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.save
+      redirect_to @book, notice: '本を更新しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
   end
 end
