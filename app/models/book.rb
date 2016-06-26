b179c367616d5222ff9afa85642d1139103973f1
Ryo Utsunomiya <millionsnowliving@gmail.com>
2016-06-26 14:52:49 +0900

    Add validation to Book.rate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  validates :rate, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'rate must be larger than or equal to 0' do
+    book = Book.new
+    book.title = 'test'
+    book.rate = -1
+    assert book.invalid?
+    assert_equal ['must be greater than or equal to 0'], book.errors[:rate]
+  end
 end
